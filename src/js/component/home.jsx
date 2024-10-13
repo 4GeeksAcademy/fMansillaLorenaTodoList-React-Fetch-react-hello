90948b431bf2567c49647fca486d0c046bed8b82
MansillaLorena <lorenamansillag@gmail.com>
2024-10-13 23:37:01 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,45 @@
-import React from "react";
-
+import React, { useState } from "react";
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
 const Home = () => {
+	const [todos, setTodos] = useState([])
+	const [newTodo, setNewTodo] = useState("")
 	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<div className="text-center container mt-5 ">
+			<h1 className="opacity-25 fs-1">todos</h1>
+			<input className="form-control" placeholder="xxxxx" value={newTodo}
+				onChange={(e) =>
+					setNewTodo(e.target.value)
+				}
+				onKeyDown={(e) => {
+					if (e.code === "Enter") {
+						setTodos([...todos, newTodo]);
+						setNewTodo("");
+
+					}
+					console.dir(e.code)
+				}}
+			/>
+
+			{todos.map((todo, index) => {
+				return (<div className="d-flex container shadow-lg p-3 bg-body-tertiary rounded"><h1 className="me-auto">{todo}</h1>
+					<button type="button" className="btn-close mt-2
+					 me-3" aria-label="Close" onClick={() => {
+							console.log(index)
+							let copytodos = [...todos]
+							copytodos.splice(index, 1)
+							console.log(copytodos)
+							setTodos(copytodos)
+						}}></button>
+				</div>)
+			})}
+			< div className="footer d-flex fs-5 opacity-50 shadow-lg p-3 bg-body-tertiary rounded" >{todos.length}Tareas</div>
+				<div classname="firstBorder"></div>
+				<div classname="secondBorder"></div>
+		</div >
+
 	);
 };
 
